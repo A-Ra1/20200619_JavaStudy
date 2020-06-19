e3a69b812722dbd0a04a4ffcafb4cd563df7d6cf
SIST <SIST@DESKTOP-TJIVCS1>
2020-06-19 14:45:40 +0900

    1

---
+++ 20200615-자바기본/src/MainClass1.java
@@ -0,0 +1,59 @@
+
+public class MainClass1 {
+   public static void main(String[] args) {
+	 //표현 => 자바
+	 /*
+	  * 정수
+	  *  = 10진법  10,20...
+	  *  = 8진법 
+	  *     011 => 9
+	  *     =
+	  *  = 16진법
+	  *     0x10
+	  *     ==
+	  *  = 2진법
+	  *     0b1010
+	  *     ==
+	  * 실수
+	  *  = 저장 크기 => 4byte 10.5f (소수점 이하 8자리)
+	  *  = 저장 크기 => 8byte 10.5  (소수점 이하 16자리)
+	  * 문자
+	  *  = ' ' => 한글자만 저장
+	  * 문자열 
+	  *  = " " => 여러글자 저장
+	  * ASC (c) ==> 1byte
+	  * Unicode (자바) => 2byte
+	  * UTF-8 => 브라우저에서 한글로 읽어야 할 때
+	  */
+	  System.out.println('A');
+	  System.out.println('홍');
+	  //한글도 가능 
+	  System.out.println("Hello");
+	  System.out.println(10.5);
+	  System.out.println(10.5f);
+	  System.out.println(10.5F);
+	  
+	  System.out.println(10); // 명령프롬프트에 10을 출력
+	  System.out.println(0b01010);
+	  System.out.println(012);
+	  System.out.println(0xA);
+	  /*
+	   *  1.기본 문법
+	   *  ===========
+	   *   => 대소문자 구분
+	   *      => 라이브러리 (클래스:대소문자 , 메소드는 소문자, 상수는 모두 대문자)
+	   *      PI = 3.14
+	   *      
+	   *      변수/상수
+	   *      
+	   *      int kor=100;      <= 변수
+	   *     final int KOR=200; <= 상수
+	   *     => 명령문이 종료가 되면 반드시 ; 
+	   *  2.들여쓰기   
+	   */
+	  
+
+	  
+	  
+   }
+}
