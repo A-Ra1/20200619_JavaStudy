d9853e3c48765bc61b46aa8a857ccc009d842fdf
SIST <SIST@DESKTOP-TJIVCS1>
2020-07-08 17:32:43 +0900

    1

---
+++ 20200708-자바클래스활용/src/Movie.java
@@ -0,0 +1,34 @@
+
+public class Movie {
+
+	/*
+class MovieInfo{
+	
+	String title;
+	String genre;
+	String director;
+	String actor;
+	int rank;
+	int runningtime;	
+
+	
+}
+*/
+	
+/*
+class StoreInfo{
+	String adress;
+	String number;
+	String price;
+	String menu;
+	String closeday;
+	
+	
+}
+*/
+class MusicInfo{
+	int rank;
+	String state;
+	
+}
+}
