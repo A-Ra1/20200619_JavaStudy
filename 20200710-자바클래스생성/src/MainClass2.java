0cd1a4cde104904e8c8237fdef357fe0d2960e83
SIST <SIST@DESKTOP-TJIVCS1>
2020-07-10 17:24:57 +0900

    1

---
+++ 20200710-자바클래스생성/src/MainClass2.java
@@ -0,0 +1,22 @@
+/*
+ *  클래스의 구성요소
+ *    메소드 : 기능, 행위, 동작
+ *      자판기
+ *        동전, 음료수
+ *        동전 투입시 처리
+ *        버튼 눌렀을시 처리
+ *        
+ */
+public class MainClass2 {
+
+	public static void main(String[] a) {
+		// TODO Auto-generated method stub
+		System.out.println("aaa");
+/*
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		*/
+	}
+
+}
